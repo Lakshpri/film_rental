1b61ba3fd2a7b17b5caa1f1c28f7f3a3e0246419
Madhumita-S03 <madhumitasiva2005@gmail.com>
2026-04-13 21:58:25 +0530

    changes done

--- flim_rental_app/src/main/java/com/example/flim_rental_app/entity/FilmActor.java
+++ flim_rental_app/src/main/java/com/example/flim_rental_app/entity/FilmActor.java
@@ -1,16 +1,24 @@
 package com.example.flim_rental_app.entity;
 
+
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Data @NoArgsConstructor @AllArgsConstructor
+@Table(name = "film_actor")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString(exclude = {"actor", "film"})
 public class FilmActor {
+
+    @EmbeddedId
+    private FilmActorId id;
 
     private LocalDateTime lastUpdate;
 
-    private Film film;
+    private Actor actor;
 
+    private Film film;
 }
+
