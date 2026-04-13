bd53e5b84424ff53ecaec526c1a77f81bab82085
Madhumita-S03 <madhumitasiva2005@gmail.com>
2026-04-13 19:29:21 +0530

    Added film

---
+++ flim_rental_app/src/main/java/com/example/flim_rental_app/entity/FilmActorId.java
@@ -0,0 +1,14 @@
+package com.example.flim_rental_app.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode
+public class FilmActorId {
+    private Integer actorId;
+    private Integer filmId;
+}
