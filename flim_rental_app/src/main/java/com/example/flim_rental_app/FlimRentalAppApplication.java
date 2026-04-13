83d15aa432ad85f6b688899fea2d0c0d9f1e062e
Lakshmi Priya <lakshpri@gmail.com>
2026-04-13 15:58:39 +0530

    Initial commit

---
+++ flim_rental_app/src/main/java/com/example/flim_rental_app/FlimRentalAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.flim_rental_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlimRentalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlimRentalAppApplication.class, args);
+	}
+
+}
