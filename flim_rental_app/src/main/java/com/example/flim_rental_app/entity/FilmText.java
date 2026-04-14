785946d7dbc50505aa5481fa7838116d2440f8b5
Subbalakshmi BV <bvsubbalakshmi1805@gmail.com>
2026-04-14 16:02:50 +0530

    FilmCategory, FilmCategoryId, FilmText, Renatal entity added

--- flim_rental_app/src/main/java/com/example/flim_rental_app/entity/FilmText.java
+++ flim_rental_app/src/main/java/com/example/flim_rental_app/entity/FilmText.java
@@ -1,10 +1,23 @@
 package com.example.flim_rental_app.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 @Entity
+@Table(name = "film_text")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class FilmText {
     @Id
     @Column(name = "film_id")
     private Integer filmId;
+
+    @NotBlank
+    @Size(max = 255)
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
 }
