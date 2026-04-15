09d05aa3cc266772f1bc331eacd0cef718c6f90c
PADMAPRABHA K <padmaprabha2005@gmail.com>
2026-04-15 11:09:41 +0530

    folder structure modified

---
+++ flim_rental_app/src/main/java/com/example/film_rental_app/entity/FilmCatalog_ContentModule/FilmActor.java
@@ -0,0 +1,38 @@
+package com.example.film_rental_app.entity.FilmCatalog_ContentModule;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "film_actor", indexes = {
+        @Index(name = "idx_fk_film_id", columnList = "film_id")
+})
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString(exclude = {"actor", "film"})
+public class FilmActor {
+
+    @EmbeddedId
+    private FilmActorId id;
+
+    @UpdateTimestamp
+    @Column(name = "last_update", nullable = false)
+    private LocalDateTime lastUpdate;
+
+    // Many FilmActor -> One Actor
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("actorId")
+    @JoinColumn(name = "actor_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_film_actor_actor"))
+    private Actor actor;
+
+    // Many FilmActor -> One Film
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("filmId")
+    @JoinColumn(name = "film_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_film_actor_film"))
+    private Film film;
+}
+
+
---
+++ flim_rental_app/src/main/java/com/example/film_rental_app/entity/FilmCatalog_ContentModule/FilmCategoryId.java
@@ -0,0 +1,15 @@
+package com.example.film_rental_app.entity.FilmCatalog_ContentModule;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode
+public class FilmCategoryId implements Serializable {
+
+    private Integer filmId;
+    private Integer categoryId;
+}
