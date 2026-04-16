004ad19cdc9a9c30730f93644d4ae8db3dd32593
Lakshmi Priya <lakshpri@gmail.com>
2026-04-16 10:21:17 +0530

    custom query in payment repository

--- flim_rental_app/src/main/java/com/example/film_rental_app/Payment_ReportsModule/repository/PaymentRepository.java
+++ flim_rental_app/src/main/java/com/example/film_rental_app/Payment_ReportsModule/repository/PaymentRepository.java
@@ -1,13 +1,21 @@
 package com.example.film_rental_app.Payment_ReportsModule.repository;
 
-
 import com.example.film_rental_app.Payment_ReportsModule.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+
+    // Already existing
     List<Payment> findByCustomer_CustomerId(Integer customerId);
+
+    // Find payments greater than amount
+    List<Payment> findByAmountGreaterThan(BigDecimal amount);
+
+    //  Find payments by staff
+    List<Payment> findByStaff_StaffId(Integer staffId);
 }
